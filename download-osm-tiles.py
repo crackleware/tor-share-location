5aecf8d5928b148f60198a668b00a3b22dc68aae
crackleware <pedja@crackleware.nu>
2019-02-06 01:04:47 +0100

    download tiles: current location by termux-location

--- download-osm-tiles.py
+++ download-osm-tiles.py
@@ -4,16 +4,23 @@ import sys
 import os
 import requests
 import time
+import json
+import subprocess
 
 from common import tile_deg2num
 
 outdir = 'maps/tiles'
 
 print('Select location:')
-print('1) by city name')
-print('2) by latitude and longitude')
+print('1) by termux-location command (current location)')
+print('2) by city name')
+print('3) by latitude and longitude')
 r = input('> ').strip()
 if   r == '1':
+    d = json.loads(subprocess.check_output('termux-location', shell=True))
+    lat, lon = d['latitude'], d['longitude']
+    print('latitude and longitude: %s, %s' % (lat, lon))
+elif r == '2':
     cities = [s.split('\t') for s in open('cities.tsv').read().split('\n') if s][1:]
     cities = sorted(cities, key=lambda c: c[0])
     for i, r in enumerate(cities):
@@ -22,7 +29,7 @@ if   r == '1':
     name_en, name, country, lat, lon = cities[int(input('> '))-1]
     lat, lon = float(lat), float(lon)
     print('%s (%s), %s - latitude and longitude: %s, %s' % (name_en, name, country, lat, lon))
-elif r == '2':
+elif r == '3':
     r = input('Latitude and longitude (ex: 12.345, 56.234): ')
     lat, lon = [float(s) for s in r.replace(',', ' ').split()
                 if s]
@@ -42,22 +49,27 @@ else: r = 50
 xrng = (x-r//2, x+r//2)
 yrng = (y-r//2, y+r//2)
 
-for z in [6, 9, 12, 15]:
-    k = 2**(zmax-z)
-    xmin, xmax = xrng[0]//k, xrng[1]//k
-    ymin, ymax = yrng[0]//k, yrng[1]//k
-    for x in range(xmin, xmax+1):
-        for y in range(ymin, ymax+1):
-            od = outdir+'/%d' % z
-            os.makedirs(od, exist_ok=True)
-            fn = od+'/%d-%d.png' % (x, y)
-            if not os.path.exists(fn):
-                print('downloading', fn)
-                try:
-                    d = requests.get('https://a.tile.openstreetmap.org/%d/%d/%d.png' % (z, x, y))
-                    with open(fn+'.tmp', 'wb+') as f: f.write(d.content)
-                    os.rename(fn+'.tmp', fn)
-                except Exception as e:
-                    print(e)
-                time.sleep(1)
+def tiles_iter():
+    for z in [6, 9, 12, 15]:
+        k = 2**(zmax-z)
+        xmin, xmax = xrng[0]//k, xrng[1]//k
+        ymin, ymax = yrng[0]//k, yrng[1]//k
+        for x in range(xmin, xmax+1):
+            for y in range(ymin, ymax+1):
+                yield z, x, y
+
+cnt = len(list(tiles_iter()))
+for i, (z, x, y) in enumerate(tiles_iter()):
+    od = outdir+'/%d' % z
+    os.makedirs(od, exist_ok=True)
+    fn = od+'/%d-%d.png' % (x, y)
+    if not os.path.exists(fn):
+        print('downloading (%d/%d)' % (i+1, cnt), fn)
+        try:
+            d = requests.get('https://a.tile.openstreetmap.org/%d/%d/%d.png' % (z, x, y))
+            with open(fn+'.tmp', 'wb+') as f: f.write(d.content)
+            os.rename(fn+'.tmp', fn)
+        except Exception as e:
+            print(e)
+        time.sleep(1)
 
